c0c5b5e793a2202107d8bb4de82e81e460366240
Eric Gjertsen <ericgj72@gmail.com>
2013-06-10 00:22:06 -0400

    initial commit

---
+++ Readme.md
@@ -0,0 +1,16 @@
+
+# editable-record
+
+  Simple in-place editing integrated with component/model
+
+## Installation
+
+    $ component install ericgj/editable-record
+
+## API
+
+   
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,118 @@
+var Emitter = require('emitter')
+  , K       = require('k')
+
+module.exports = EditableRec;
+
+function EditableRec(el, model){
+ if (!(this instanceof EditableRec)) return new EditableRec(el,model);
+ this.model = model;
+ this.editables = [];
+ this.doneKeys = "enter";
+ this.cancelKeys = "esc";
+ if (el) this.init(el);
+ this.keys = K(el);
+ return this;
+}
+
+EditableRec.prototype = new Emitter;
+
+EditableRec.prototype.editing = function(){
+  return this._editing;
+}
+
+EditableRec.prototype.edit = function(){
+  if (this._editing) return this;
+  this._keybind();
+  this._editing = true;
+  for (var i=0;i<this.editables.length;++i) this.editables[i].edit();
+  this.emit('edit', this.model);
+  return this;
+}
+
+EditableRec.prototype.done = function(){
+  if (!this._editing) return this;
+  for (var i=0;i<this.editables.length;++i) this.editables[i].done();
+  this.emit('done', this.model);
+  if (this.model.save) this.model.save();
+  this._editing = false;
+  this._keyunbind();
+  return this;
+}
+
+EditableRec.prototype.cancel = function(){
+  if (!this._editing) return this;
+  for (var i=0;i<this.editables.length;++i) this.editables[i].cancel();
+  this.emit('cancel', this.model);
+  this._editing = false;
+  this._keyunbind();
+  return this;
+}
+
+EditableRec.prototype.addEditable = function(el,attr){
+  this.editables.push(new EditableAttr(el,this,attr));
+  return this;
+}
+  
+EditableRec.prototype.init = function(el){
+  el = (typeof el == "string" ? document.querySelector(el) : el);
+  var eds = el.querySelectorAll('.editable');
+  for (var i=0; i<eds.length; ++i){
+    var ed = eds[i],
+        attr = data(ed)["model-attr"];
+    if (attr) this.addEditable(ed,attr);
+  }
+  return this;
+}
+
+EditableRec.prototype._keybind = function(){
+  if (this.doneKeys)   this.keys.bind(this.doneKeys, this.done.bind(this));
+  if (this.cancelKeys) this.keys.bind(this.cancelKeys, this.cancel.bind(this));
+}
+
+EditableRec.prototype._keyunbind = function(){
+  if (this.doneKeys)   this.keys.unbind(this.doneKeys);
+  if (this.cancelkeys) this.keys.unbind(this.cancelKeys);
+}
+
+
+
+function EditableAttr(el,rec,attr){
+  this.el = el;
+  this.attr = rec.model[attr].bind(rec.model);
+}
+
+EditableAttr.prototype.edit = function(){
+  this.el.innerHTML = this.attr();
+  this.el.setAttribute('contentEditable', true);
+}
+
+EditableAttr.prototype.done = function(){
+  this.el.removeAttribute('contentEditable');
+  this.el.blur();
+  if (this.attr() !== this.el.innerHTML){
+    this.attr(this.el.innerHTML);
+  }
+}
+
+EditableAttr.prototype.cancel = function(){
+  this.el.innerHTML = this.attr();
+  this.done();
+}
+
+// private 
+
+var data = function(el){
+  var ret = {};
+  for (var i=0; i<el.attributes.length; ++i){
+    var attr = el.attributes[i]
+      , key = attr.name
+      , parts = key.split('-')
+      , prefix = parts.shift()
+      , rest = parts.join('-');
+    if (prefix == "data" && rest.length > 0){
+      ret[rest] = attr.value;
+    }
+  }
+  return ret;
+}
+
